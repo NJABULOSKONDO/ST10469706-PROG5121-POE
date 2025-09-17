98205a5ff6869bb09668997123bc270851756836
Njabulo <ST104697@rcconnect.edu.za>
2025-09-17 13:12:16 +0200

    feat: add User class with fields for username, password, and phone number

---
+++ src/main/java/com/mycompany/st10469706/ST10469706.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.st10469706;
+
+/**
+ *
+ * @author Njabulo Skondo
+ */
+public class ST10469706 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/com/mycompany/st10469706/ST10469706Test.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package com.mycompany.st10469706;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Vavi Fuyego
+ */
+public class ST10469706Test {
+    
+    public ST10469706Test() {
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testMain() {
+    }
+    
+}
