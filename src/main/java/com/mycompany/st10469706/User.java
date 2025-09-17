17b60ce970b38dfaca56b34fa82d29b5e530ee86
Njabulo <ST104697@rcconnect.edu.za>
2025-09-17 12:51:00 +0200

    feat: add User class with fields for username, password, and phone number

---
+++ src/main/java/com/mycompany/st10469706/User.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.st10469706;
+
+/**
+ *
+ * @author Njabulo Skondo
+ */
+public class User {
+    private String username;
+    private String password;
+    private String phoneNumber;
+    
+    public User(String username, String password, String phoneNumber) {
+        this.username = username;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+    }
+    
+    // Getters and setters
+}
