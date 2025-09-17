f749b9fa1c217771e02fabc80e22d5106d94fc2d
Njabulo <ST104697@rcconnect.edu.za>
2025-09-17 14:55:19 +0200

    feat: build CLI interface for user registration and login in Main class

--- src/main/java/com/mycompany/st10469706/ST10469706.java
+++ src/main/java/com/mycompany/st10469706/ST10469706.java
@@ -8,9 +8,29 @@ package com.mycompany.st10469706;
  *
  * @author Njabulo Skondo
  */
+import java.util.Scanner;
+
 public class ST10469706 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+        UserService service = new UserService();
+        
+        System.out.println("Enter username: ");
+        String username = scanner.nextLine();
+        System.out.println(service.validateUsername(username));
+        
+        System.out.print("Enter password: ");
+        String password = scanner.nextLine();
+        System.out.println(service.validatePassword(password));
+        
+        System.out.print("Enter South African number: ");
+        String phone = scanner.nextLine();
+        
+        if (service.registerUser(username, password, phone)) {
+            System.out.println("Registration Successful!");
+        } else {
+            System.out.println("Registration failed. Please check your inputs.");
+        }
     }
 }
