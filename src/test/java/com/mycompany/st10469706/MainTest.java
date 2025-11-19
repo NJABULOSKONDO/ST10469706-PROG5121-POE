68de392115a8c555b08c3489a061e57880723a0f
Njabulo <ST104697@rcconnect.edu.za>
2025-11-19 13:33:56 +0200

    Added missing import for List and ArrayList in MainTest

--- src/test/java/com/mycompany/st10469706/MainTest.java
+++ src/test/java/com/mycompany/st10469706/MainTest.java
@@ -6,6 +6,8 @@ package com.mycompany.st10469706;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+import java.util.ArrayList;
 
 /**
  *
